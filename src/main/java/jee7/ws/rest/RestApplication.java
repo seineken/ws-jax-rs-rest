faad40522fffdb0f8335684b9f5ead125681559e
tecris <eugen@cristea.org>
2015-03-12 16:03:02 +1300

    removed old code

--- src/main/java/jee7/ws/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package jee7.ws.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-}
